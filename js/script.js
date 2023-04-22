a72579483ce6632d1fa34c34b1757ec00188eb2b
Cristian Cabrera <cristiancabrera@Cristians-iMac-3.home>
2023-04-21 20:32:53 -0700

    added displayMovieDetails() & addCommasToNumber()

--- js/script.js
+++ js/script.js
@@ -89,6 +89,68 @@ async function displayPopularShows() {
   console.log(results)
 }
 
+// display movie details
+async function displayMovieDetails() {
+  const movieID = window.location.search.split('=')[1]
+
+  const movie = await fetchAPIData(`movie/${movieID}`)
+
+  const div = document.createElement('div')
+
+  div.innerHTML = `
+        <div class="details-top">
+          <div>${
+            movie.poster_path ? 
+            `
+              <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />
+            ` :
+            `
+            <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+            />
+            `
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+            ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}</div>
+        </div>
+  `
+
+  document.querySelector('#movie-details').appendChild(div)
+
+  // console.log(movie)
+}
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
@@ -127,6 +189,10 @@ function highlightActiveLink(){
   } )
 }
 
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
 
 // init app
 function init() {
@@ -139,7 +205,7 @@ function init() {
     displayPopularShows()
       break
     case '/011-flixx-app/movie-details.html':
-      console.log('movie details')
+    displayMovieDetails()
       break
     case '/011-flixx-app/tv-details.html':
       console.log('tv details')
