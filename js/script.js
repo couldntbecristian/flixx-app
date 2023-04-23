2a5a23f25127eca1832836b82c8d6e934db1935c
Cristian Cabrera <cristiancabrera@Cristians-iMac-3.home>
2023-04-22 19:22:52 -0700

    added displayShowDetails()

--- js/script.js
+++ js/script.js
@@ -115,7 +115,7 @@ async function displayMovieDetails() {
             <img
               src="images/no-image.jpg"
               class="card-img-top"
-              alt="Movie Title"
+              alt="${movie.title}"
             />
             `
           }
@@ -152,7 +152,74 @@ async function displayMovieDetails() {
 
   document.querySelector('#movie-details').appendChild(div)
 
-  console.log(movie)
+  // console.log(movie)
+}
+
+// display show details
+async function displayShowDetails() {
+  const showID = window.location.search.split('=')[1]
+
+  const show = await fetchAPIData(`tv/${showID}`)
+
+// Overlay for background image
+  displayBackgroundImage('tv', show.backdrop_path)
+
+  const div = document.createElement('div')
+
+  div.innerHTML = `
+        <div class="details-top">
+          <div>${
+            show.poster_path ? 
+            `
+              <img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />
+            ` :
+            `
+            <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />
+            `
+          }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${show.first_air_date}</p>
+            <p>
+            ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${show.last_air_date} 
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}</div>
+        </div>
+  `
+
+  document.querySelector('#show-details').appendChild(div)
+
+  console.log(show)
 }
 
 // Display backdrop on details page
@@ -233,7 +300,7 @@ function init() {
       displayMovieDetails()
       break
     case '/011-flixx-app/tv-details.html':
-      console.log('tv details')
+      displayShowDetails()
       break
     case '/011-flixx-app/search.html':
     console.log('search')
