ad51491b89e82db74716a8fd4787dd4b73eb8469
Cristian Cabrera <cristiancabrera@Cristians-iMac-3.home>
2023-04-22 22:21:41 -0700

    added displaySlider() using the swiper library from https://swiperjs.com/

--- js/script.js
+++ js/script.js
@@ -244,6 +244,52 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Display slider movies
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie//now_playing')
+
+  results.forEach(movie => {
+    const div = document.createElement('div')
+    div.classList.add('swiper-slide')
+
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>    
+    `
+    document.querySelector('.swiper-wrapper').appendChild(div)
+
+    initSwiper()
+  })
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1, 
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2
+      },
+      700: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  })
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
 
@@ -292,6 +338,7 @@ function init() {
     case '/011-flixx-app/':
     case '/011-flixx-app/index.html': 
       displayPopularMovies()
+      displaySlider()
       break
     case '/011-flixx-app/shows.html':
       displayPopularShows()
